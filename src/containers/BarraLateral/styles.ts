c983215b57b3b5ea1040497929304ac26ee10ce3
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-09-07 14:03:41 -0300

    compoente filtro e container barra lateral

---
+++ src/containers/BarraLateral/styles.ts
@@ -0,0 +1,23 @@
+import { styled } from 'styled-components'
+
+export const Aside = styled.aside`
+  padding: 16px;
+  height: 100vh;
+  background-color: #eee;
+`
+
+export const Filtros = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 8px;
+  margin-top: 16px;
+`
+
+export const Campo = styled.input`
+  width: 100%;
+  border-radius: 8px;
+  padding: 8px;
+  font-weight: bold;
+  background-color: #fff;
+  border-color: #666666;
+`
