b724430687ad78711469a837702071b8eb162a9a
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-05-11 00:12:57 -0300

    redux initial config

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,45 +1,31 @@
+import { useSelector } from 'react-redux'
+
+import type { RootReducer } from '../../store'
 import Tarefa from '../../components/Tarefa'
-
 import { Container } from './styles'
-import * as enums from '../../utils/enums/Task'
 
-const tasks = [
-  {
-    titulo: 'Estudar Typescript',
-    description: 'Ver a aula 3 do curso',
-    priority: enums.Priority.IMPORTANTE,
-    status: enums.Status.CONCLUIDA
-  },
-  {
-    titulo: 'Ir na padaria',
-    description: 'comprar pão',
-    priority: enums.Priority.URGENTE,
-    status: enums.Status.PENDENTE
-  },
-  {
-    titulo: 'Ir para academia',
-    description: 'treino de perna',
-    priority: enums.Priority.NORMAL,
-    status: enums.Status.CONCLUIDA
-  }
-]
+const ListaDeTarefas = () => {
+  const { tasks } = useSelector((state: RootReducer) => state)
 
-const ListaDeTarefas = () => (
-  <Container>
-    <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
-    <ul>
-      {tasks.map((t) => (
-        <li key={t.titulo}>
-          <Tarefa
-            title={t.titulo}
-            description={t.description}
-            priority={t.priority}
-            status={t.status}
-          />
-        </li>
-      ))}
-    </ul>
-  </Container>
-)
+  return (
+    <Container>
+      <p>
+        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
+      </p>
+      <ul>
+        {tasks.map((t) => (
+          <li key={t.title}>
+            <Tarefa
+              title={t.title}
+              description={t.description}
+              priority={t.priority}
+              status={t.status}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
 
 export default ListaDeTarefas
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -3,14 +3,14 @@ import * as enums from '../utils/enums/Task'
 class Task {
   title: string
   priority: enums.Priority
-  state: enums.Status
+  status: enums.Status
   description: string
   id: number
 
-  constructor(title: string, priority: enums.Priority, state: enums.Status, description: string, id: number) {
+  constructor(title: string, priority: enums.Priority, status: enums.Status, description: string, id: number) {
     this.title = title
     this.priority = priority
-    this.state = state
+    this.status = status
     this.description = description
     this.id = id
   }
