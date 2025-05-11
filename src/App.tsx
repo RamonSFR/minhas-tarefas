3651f21ee701c2bc3c0095dc2255dfca5f96e9a3
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-05-11 16:17:30 -0300

    router dom initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import store from './store'
 
-import BarraLateral from './containers/BarraLateral'
-import ListaDeTarefas from './containers/ListaDeTarefas'
 import EstiloGlobal, { Container } from './styles'
+import Home from './pages/Home'
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <h1>Nova Tarefa</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={routes} />
       </Container>
     </Provider>
   )
